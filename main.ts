d734686c114011a6671dd67f0d55b8be7a57b4f9
Taegyn <114882775+Taegyn@users.noreply.github.com>
2022-10-02 15:30:46 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
-basic.forever(function () {
-	
+let Whatever_i_want = 0
+let i_want_this = 0
+input.onButtonPressed(Button.A, function () {
+    Whatever_i_want = randint(0, 1)
+    if (Whatever_i_want == 0) {
+        basic.showString("Dare")
+    }
+    if (Whatever_i_want == 1) {
+        basic.showString("Truth")
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    i_want_this = randint(0, 3)
+    if (i_want_this == 0) {
+        basic.showArrow(ArrowNames.North)
+    } else if (i_want_this == 1) {
+        basic.showArrow(ArrowNames.South)
+    } else if (i_want_this == 2) {
+        basic.showArrow(ArrowNames.East)
+    } else if (i_want_this == 3) {
+        basic.showArrow(ArrowNames.West)
+    }
 })
